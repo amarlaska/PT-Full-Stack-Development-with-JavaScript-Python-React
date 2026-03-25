89f18bed169c77d5b7e658bdcf64109484c5ccc9
amarlaska <albamarlaska@example.com>
2026-03-25 17:35:14 +0100

    3 checkpoint

---
+++ Entregables/Checkpoint 3/assignment.py
@@ -0,0 +1,51 @@
+#Exercise 1
+
+string_one = 'Hola mundo'
+number_one = 1
+list_one = ['Alba', 'María', 'Pedro', 'Iñigo']
+boolean_one = True
+
+#Exercise 2
+
+first_three_letters = string_one[0:3]
+
+#Exercise 3
+
+first_element = list_one[0]
+
+#Exercise 4
+
+new_number = number_one + 10
+
+#Exercise 5
+
+last_element = list_one[-1]
+
+#Exercise 6
+names = 'harry,alex,susie,jared,gail,conner'
+names_list = names.split(',')
+
+#Exercise 7
+
+first_word = string_one[0:4]
+
+first_word = first_word.upper()
+
+string_two = first_word + string_one[4:]
+
+print(string_two)
+
+
+#Exercise 8 
+
+sentence = f'Mi sobrino Hugo nació el {number_one} de enero de 2001'
+print(sentence)
+
+#Exercise 9
+
+print('hello world')
+
+#Ejercicio extra
+
+greeting = 'Hola Jaime'
+greeting = greeting.replace('Hola', 'adiós')
---
+++ python/funciones.py
@@ -0,0 +1,14 @@
+sentence = 'The quick brown fox jumped'
+sentence_two = sentence.upper()
+
+print(sentence)
+print(sentence_two)
+
+sentence_three = 'the quick brown fox jumped'.title()
+print(sentence_three)
+
+sentence_four = 'THE QUICK BROWN FOX JUMPED'.lower()
+print(sentence_four)
+
+sentence_five = 'THE QUICK BROWN FOX JUMPED'
+print(sentence_five.lower())
